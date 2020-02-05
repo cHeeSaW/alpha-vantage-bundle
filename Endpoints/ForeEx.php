1d6c723533961067ba1035da02c832c8f8ac6861
Martin Tomala <cheesaw@gmx.net>
2020-02-05 13:27:15 +0100

    set defaults and add symfony dependencies

--- Endpoints/ForeEx.php
+++ Endpoints/ForeEx.php
@@ -62,7 +62,7 @@ class ForeEx implements Endpoint
      * Time interval between two consecutive data points in the time series.
      * The following values are supported: 1min, 5min, 15min, 30min, 60min
      */
-    public ?string $interval;
+    public ?string $interval = self::INTERVAL_5_MIN;
 
     /**
      * By default, datatype=json. Strings json and csv are accepted with the following specifications:
